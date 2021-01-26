57e3cc8a175ba87483a76c3df3a236f00bb085f6
adriannamckeown <71573922+adriannamckeown@users.noreply.github.com>
2021-01-26 15:54:35 -0500

    idk

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
 
 const jwt = require('jsonwebtoken');
 const exjwt = require('express-jwt');
 const bodyParser = require('body-parser');
 const path = require('path');
+const crypto = require('crypto');
+
+//MySQL database connection
+var connection = mysql.createConnection({
+    host        : 'sql9.freemysqlhosting.net',
+    user        : 'sql9374284',
+    password    : 'GHkghQTuES',
+    database    : 'sql9374284'
+});
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -15,7 +25,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 const PORT = 3000;
 
 const secretKey = 'My super secret key';
@@ -24,19 +33,29 @@ const jwtMW = exjwt({
     algorithms: ['HS256']
 });
 
-let users = [
-    {
-        id: 1,
-        username: 'adrianna',
-        password: '123'
-    },
-    {
-        id: 2,
-        username: 'mckeown',
-        password: '456'
-    }
-];
+app.post('/api/signup', async (req, res) => {
+    const {username, password} = req.body;
+    const pwd = crypto.createHash('sha256').update(password).digest('hex');
+    const date = new Date().toJSON().slice(0,10);
+    connection.connect();
+    connection.query('INSERT INTO user VALUES("", ?, ?, ?)', [username, pwd, date], function(error, results, fields) {  
+        connection.end();
+        if (error) throw error;
+        res.json(results);
+    });
+});
 
+app.get('/api/signup', async (req, res) => {
+    connection.connect();
+    connection.query('SELECT * FROM user', function(error, results, fields) {
+        connection.end();
+        if (error) throw error;
+        res.json(results)
+    });
+});
+
+
+//SELECT * users maybe????
 app.post('/api/login', (req, res) => {
     const { username, password } = req.body;
 
@@ -58,13 +77,6 @@ app.post('/api/login', (req, res) => {
             });
         }
     }
-});
-
-app.get('/api/dashboard', jwtMW, (req, res) => {
-    res.json({
-        success: true,
-        myContent: 'Secret content that only logged in people can see!!'
-    });
 });
 
 app.get('/api/settings', jwtMW, (req, res) => {
